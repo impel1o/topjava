53b8d52ce99faec194b292e0c0b4d1625146eb00
impel1o <gamer.pvs@gmail.com>
2017-05-10 14:51:45 +0300

    hw6 1 fix 1.1 , 1.2

--- src/test/java/ru/javawebinar/topjava/service/jpa/JpaUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jpa/JpaUserServiceTest.java
@@ -1,7 +1,6 @@
 package ru.javawebinar.topjava.service.jpa;
 
 import org.springframework.test.context.ActiveProfiles;
-import ru.javawebinar.topjava.service.AbstractUserJpaServiceTest;
 import ru.javawebinar.topjava.service.AbstractUserServiceTest;
 
 import static ru.javawebinar.topjava.Profiles.JPA;
